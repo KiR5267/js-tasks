688887f303d584fcd7e5a7e9f2951b668c99fa52
KiR5267 <61653610+KiR5267@users.noreply.github.com>
2020-09-17 20:09:02 +0300

    Add files via upload

---
+++ JStask33/scripts.js
@@ -0,0 +1,54 @@
+for (let a = 1; a <= 50; a++) {
+    console.log(a);
+}
+
+console.log('абзац');
+
+for (let b = 35; b >= 8; b--) {
+    console.log(b);
+}
+
+console.log('абзац');
+
+Alexander Tobolich, [17.09 .20 20: 06]
+
+
+for (let i = 1; i < 50; i++) console.log(i);
+
+
+for (let i = 35; i > 8; i--) console.log(i);
+
+
+let a = 89;
+while (a > 11) {
+    document.write($ { a } < br > );
+    a--;
+}
+
+
+let sum = 0,
+    b = 1;
+for (let b = 1; b <= 100; b++) {
+    console.log(b);
+    sum += b;
+}
+console.log(sum)
+
+
+
+for (let i = 1; i <= 5; i++) {
+    sum = 0;
+    for (let h = 1; h <= i; h++) {
+        sum += h;
+    }
+    console.log(i + " = " + sum)
+}
+
+
+
+for (let i = 8; i < 56; i++) {
+    if (i % 2 != 0) {
+        continue
+    }
+    console.log(i);
+}
