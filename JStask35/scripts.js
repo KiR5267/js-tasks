9a0e216736f28c64bbc20940bc35d4da24477d5d
KiR5267 <61653610+KiR5267@users.noreply.github.com>
2020-09-22 20:18:55 +0300

    Add files via upload

---
+++ JStask35/scripts.js
@@ -0,0 +1,44 @@
+/*task 1
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}*/
+
+
+
+/*task6
+let fruits = ['Саша', 'Витя', 'Коля'];
+console.log(fruits);
+fruits[fruits.length] = 'Редиска';
+console.log(fruits);*/
+
+/*task2
+
+let arr10 = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7]
+for (let i = 0; i <arr10.length; i++) {
+    if (arr10[i] < 0&& arr10[i]  > -10 && arr10[i] < -3)
+}*/
+
+/*task4
+let arr55 = [];
+arr =  [‘10’, ‘20’, ‘30’, ‘50’, ‘235’, ‘3000’];
+for(let i = 0; i<=arr.length -1; i++) {
+    arr55.push(+arr[i])
+}
+console.log(typeof arr55, arr55filter(num=>['1', '2', '5'].includes(num.tostring()[0])))*/
+
+/*task5
+let  a = 'ПН, ВТ, СР, ЧТ, ПТ, СБ, ВС'
+ arr1 = a.split(','),
+ arr2 = [0],
+ arr3= [0];
+ for(let i= 0; i<arr1.lenght; i++){
+     arr2.push(arr1[i])
+ }
+ for (let i=1; i <arr2.length -2;i++){
+     arr3push(arr2[i])
+ }
+ document.write text `&(arr3[i]<br>`);
+ for (let i= 1; i<arr2.length; i=++){
+     if(arr7.length - 1==i || arr7.length - 2==i)
+     document.write(text <strong style="color red">${arr2[i]}</strong><br>)
+ }*/
