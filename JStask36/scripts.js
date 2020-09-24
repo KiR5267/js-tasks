c5fa3e4b3b954e163b87f088919a0da89e0830e3
KiR5267 <61653610+KiR5267@users.noreply.github.com>
2020-09-24 19:02:34 +0300

    Add files via upload

---
+++ JStask36/scripts.js
@@ -0,0 +1,30 @@
+/*task1
+let i = function(a, b, c) {
+    return (a - b) / c;
+}
+console.log(i(20, 6, 2));*/
+
+
+/*task5
+function isEven(num) {
+    let x
+    if (num % 5 == 0) {
+        x = true;
+    } else {
+        x = false;
+    }
+    return x;
+
+}
+
+alert(isEven(20));*/
+
+/*tack6
+let arr = [1, 2, 7, 9, 11, 24, 58, 67, 70, 72, 88, 95, 104, 253, 478, 647, 789, 991, 995, 998];
+let Arr1 = [];
+for (let i = 0; i <= arr.length; i++) {
+    if (isEven(arr[i])) {
+        Arr1.push(arr[i]);
+    }
+}
+console.log(Arr1);*/
