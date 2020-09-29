de28d952fddd93450480e5c48a7b9af66776e5ad
KiR5267 <61653610+KiR5267@users.noreply.github.com>
2020-09-29 18:13:09 +0300

    Add files via upload

---
+++ JStask37/scripts.js
@@ -0,0 +1,71 @@
+ //прошлые задания
+ /*task 2
+
+    function cub(num) {
+        return num * num * num;
+    }
+    console.log(cub(4));*/
+
+
+ /*task 3
+
+ let a = prompt('число 1'),
+     b = prompt('число 2');
+ c = Math.max(a, b);
+ d = Math.min(a, b)
+ console.log('max' + c);
+ console.log('min' + d);*/
+
+ //-------------------------------------//
+
+ //newwww
+
+
+ /*Задание 1
+ let str = 'aaa@bbb@ccc';
+ alert(str.replace(/@/g, '!'));*/
+
+
+
+ /*Задание 2
+ let str = '2025-12-31';
+ let arr = str.split('-');
+ let newStr = arr[2] + '/' + arr[1] + '/' + arr[0];
+ alert(newStr);*/
+
+
+
+
+ /* Задание 3
+ let str = 'Я учу javascript!';
+
+ console.log(str.substring(2, 5));
+ console.log(str.substring(6, 16));
+
+ console.log(str.substr(2, 4));
+ console.log(str.substr(6, 10));
+
+ console.log(str.slice(2, 5));
+ console.log(str.slice(6, 16));*/
+
+ /*Задание 4
+ let arr = [4, 2, 5, 19, 13, 0, 10];
+ let b = 0;
+ for (let i = 0; i < arr.length; i++) {
+     b += Math.pow(arr[i], 3);
+ }
+ alert('Квадратный корень - ' + Math.sqrt(b));*/
+
+
+
+
+ /*Задание 5 
+ let a = 3;
+ let b = 5;
+ let c = Math.abs(a - b);
+ alert(c);
+
+ let a = 6;
+ let b = 1;
+ let c = Math.abs(a - b);
+ alert(c);*/
